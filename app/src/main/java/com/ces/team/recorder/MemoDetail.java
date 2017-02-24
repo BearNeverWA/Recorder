6b7f116e296bf8089eef6c32238990e524a458f8
xiongxiongxiong8718 <915968268@qq.com>
2017-02-24 19:31:51 +0800

    备忘详情页面设计

--- app/src/main/java/com/ces/team/recorder/MemoDetail.java
+++ app/src/main/java/com/ces/team/recorder/MemoDetail.java
@@ -2,12 +2,48 @@ package com.ces.team.recorder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MemoDetail extends AppCompatActivity {
-
+public class MemoDetail extends AppCompatActivity implements View.OnClickListener{
+    Button btnSave,btnDelete,btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_detail);
+        btnSave= (Button) findViewById(R.id.btn_save_edit_memo);
+        btnSave.setOnClickListener(this);
+        btnDelete= (Button) findViewById(R.id.btn_delete_memo);
+        btnDelete.setOnClickListener(this);
+        btnBack= (Button) findViewById(R.id.btn_back_activity_main);
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_back_activity_main:
+                finish();
+                break;
+            case R.id.btn_save_edit_memo:
+                //这里添加修改数据库的操作,可以用DB的update方法
+                if (isMemoChanged())
+                    finish();
+                else{
+                    Toast.makeText(MemoDetail.this,"Database has updated,from MemoDetail.java",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btn_delete_memo:
+                //数据库delete操作
+                Toast.makeText(MemoDetail.this,"Delete completed",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+    }
+
+    private boolean isMemoChanged(){
+        double t=Math.random();
+        return t>=0.5;
     }
 }
